52c3dbf51f511362c31455da65504724a20df2d3
Alina Laniuk <alinavoronina16@gmail.com>
2026-03-23 17:26:52 +0300

    feat: add dashboard colors

---
+++ src/constants/colors.ts
@@ -0,0 +1,24 @@
+export const eventsColors: Record<string, string> = {
+  WORK: '#b6d635',
+  STOP: '#fb668d',
+  LOW_SPEED: '#ff994d',
+  STANDARD_OPERATION: '#535673',
+  CIP: '#0da8df',
+};
+
+export const productsColors: Record<string, string> = {
+  p1: '#299bc4',
+  p2: '#fb668d',
+  p3: '#5670db',
+  p4: '#ffd82f',
+  p5: '#d796de',
+  p6: '#bbdeee',
+};
+
+export const themeColors = {
+  primary: '#48c199',
+  danger: '#fb668d',
+  primaryLine: '#0da8df',
+  secondaryLine: '#535673',
+  backgroundDanger: '#fb668d',
+};
